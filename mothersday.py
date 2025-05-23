7bf120dbfc244a7475ad85bfb2dcc908e0fc44c2
AYLATHEPOOKIE <ayla@mansoor.uk>
2025-05-23 17:16:27 +0100

    fjoe

--- mothersday.py
+++ mothersday.py
@@ -2,9 +2,11 @@ import pgzrun,random
 WIDTH=800
 HEIGHT=600
 TITLE="mothers dayyyyyyyyyyy"
-score=0
+score=
+totalq=0
+qlist=[]
 
-questionrect=Rect(10,100,670,150)
+questionrect=Rect(10,100,790,150)
 answerrect1=Rect(10,290,335,115)
 answerrect2=Rect(350,290,335,115)
 answerrect3=Rect(10,415,335,115)
@@ -15,15 +17,20 @@ person=Actor("bottle")
 person.pos=(400,500)
 
 def draw():
-    screen.fill((50,0,35))
-    screen.draw.filled_rect(questionrect,(41,2,25))
-    screen.draw.filled_rect(answerrect1,(107,4,45))
-    screen.draw.filled_rect(answerrect2,(107,4,45))
-    screen.draw.filled_rect(answerrect3,(107,4,45))
-    screen.draw.filled_rect(answerrect4,(107,4,45))
+    screen.fill((10,0,5))
+    screen.draw.filled_rect(questionrect,(10,2,25))
+    screen.draw.filled_rect(answerrect1,(67,4,45))
+    screen.draw.filled_rect(answerrect2,(67,4,45))
+    screen.draw.filled_rect(answerrect3,(67,4,45))
+    screen.draw.filled_rect(answerrect4,(67,4,45))
     person.draw()
 
-
+def listforq():
+    global qlist, totalq
+    with open("q.txt","r") as file:
+        for i in file:
+            qlist.append(i)
+            totalq+=1
 def update():
     for i in answerrect:
         i.y+=5
